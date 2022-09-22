7851089a5b04a7f3fd1ef3491dbaf8e4dd60daf6
Tomáš Macháček <tomas.machacek@inventi.cz>
2022-09-22 09:49:38 +0200

    product add new

--- app/src/Controller/ProductController.php
+++ app/src/Controller/ProductController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +17,26 @@ class ProductController extends AbstractController
     {
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
+        ]);
+    }
+
+    #[Route('/product/add', name: 'app_product_add')]
+    public function add(Request $request, ProductRepository $products): Response
+    {
+        $form =$this->createForm(ProductType::class, new Product());
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product = $form->getData();
+
+            $products->add($product, true);
+            $this->addFlash('succecs', 'Product have been added.');
+            return $this->redirectToRoute('app_product');
+        }
+
+        return $this->render('product/add.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
